8a3e196a9318f35cbfa0ce768c50d3c460a6dba1
Una Kravets <unakravets@unakravets-macbookpro2.roam.corp.google.com>
2019-04-12 13:44:39 -0700

    base login

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import {MDCRipple} from '@material/ripple/index';
+import {MDCTextField} from '@material/textfield';
+import {MDCFormField} from '@material/form-field';
+import {MDCCheckbox} from '@material/checkbox';
 
 const ripple = new MDCRipple(document.querySelector('.mdc-button'));
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
+const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
+const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+formField.input = checkbox;
